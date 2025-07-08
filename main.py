6bc56fe98b323693b701950097798a75aebd4441
Mahima Manik <mahimamanik.22@gmail.com>
2025-07-08 15:39:28 -0700

    Implement get collection list

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from mcp-server!")
+from mcp.server.fastmcp import FastMCP
+from zeroentropy import ZeroEntropy
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 
+mcp = FastMCP("zeroentropy")
+
+zeroentropy_client = ZeroEntropy(
+    api_key=getenv("ZEROENTROPY_API_KEY")
+)
+
+@mcp.tool()
+async def get_collection_list() -> list[str]:
+    response = zeroentropy_client.collections.get_list()
+    return response.collections
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport="stdio")
