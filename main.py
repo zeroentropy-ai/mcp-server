028187c9cc6472631a906ad8bf05548850d91a54
Mahima Manik <mahimamanik.22@gmail.com>
2025-07-08 16:02:32 -0700

    Add queries

--- main.py
+++ main.py
@@ -49,6 +49,43 @@ async def get_page_info(collection_name: str, path: str, page_index: int, includ
     response = zeroentropy_client.documents.get_page_info(collection_name=collection_name, path=path, page_index=page_index, include_content=include_content)
     return response.page
 
+@mcp.tool()
+async def get_top_documents(collection_name: str, query: str, k: int, filter: dict | None = None, include_metadata: bool = False, reranker: str | None = None) -> list[str]:
+    response = zeroentropy_client.queries.top_documents(
+        collection_name=collection_name, 
+        query=query, 
+        k=k, 
+        filter=filter, 
+        include_metadata=include_metadata, 
+        reranker=reranker
+    )
+    return response.results
+
+@mcp.tool()
+async def get_top_pages(collection_name: str, query: str, k: int, filter: dict | None = None, include_content: bool = False, latency_mode: str | None = None) -> list[str]:
+    response = zeroentropy_client.queries.top_pages(
+        collection_name=collection_name, 
+        query=query, 
+        k=k, 
+        filter=filter, 
+        include_content=include_content,
+        latency_mode=latency_mode
+    )
+    return response.results
+
+@mcp.tool()
+async def get_top_snippets(collection_name: str, query: str, k: int, reranker: str | None = None, filter: dict | None = None, 
+precise_responses: bool = False, include_document_metadata: bool = False) -> list[str]:
+    response = zeroentropy_client.queries.top_snippets(
+        collection_name=collection_name, 
+        query=query, 
+        k=k, 
+        reranker=reranker,
+        filter=filter, 
+        precise_responses=precise_responses,
+        include_document_metadata=include_document_metadata,
+    )
+    return response
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
